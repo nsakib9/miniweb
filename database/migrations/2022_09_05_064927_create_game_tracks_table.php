7137ad1984db95b02b7328ff241e29fc59c1ea7c
oni98 <mehedi.hassan.oni98@gmail.com>
2022-09-18 19:37:10 +0600

    foreign key changed

--- database/migrations/2022_09_05_064927_create_game_tracks_table.php
+++ database/migrations/2022_09_05_064927_create_game_tracks_table.php
@@ -15,7 +15,8 @@ class CreateGameTracksTable extends Migration
     {
         Schema::create('game_tracks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->integer('score')->nullable();
             $table->boolean('otp_mached')->nullable();
             $table->boolean('track')->nullable();
